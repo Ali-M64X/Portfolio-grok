4070cbc7f4cac97e428a0fe7c4bb6f71c66faaeb
‪Ali Maher <mly607036@gmail.com>
2025-05-02 01:59:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 
+// فتح وإغلاق القايمة بالضغط على الهمبرغر
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('active');
+});
+
+// إغلاق القايمة لما اليوزر يضغط خارجها
+document.addEventListener('click', (event) => {
+  if (!hamburger.contains(event.target) && !navLinks.contains(event.target)) {
+    navLinks.classList.remove('active');
+  }
 });
